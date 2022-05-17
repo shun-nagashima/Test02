5aa0fd1f5f553ea9b1e8b4c9cb4daef3019613be
onebpm <onebpm@gmail.com>
2022-05-17 17:27:27 +0900

    タイプミスを修正

--- Class03.py
+++ Class03.py
@@ -1,23 +1,24 @@
 class Student:
      def __init__(self,name): 
         self.name = name
-     def calculate_avg(self,data): sum = 0
-     
-     for num in data:
-         sum += num
-         
-         avg = sum/len(data)
-         return avg
+
+     def calculate_avg(self,data): 
+        sum = 0
+        for num in data:
+            sum += num
+            
+            avg = sum/len(data)
+            return avg
      
      def judge(self,avg):
          
-          if(avg >= 60):
-              result ="passe"
-               else:
-             result ="failed"
-             return result
+        if(avg >= 60):
+            result ="passe"
+        else:
+            result ="failed"
+            return result
          
-a001 = student("sato")
+a001 = Student("sato")
 data = [70,65,50,90,30]
 avg = a001.calculate_avg(data)
 result = a001.judge(avg)
